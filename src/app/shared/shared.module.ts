9e66625aac20d44d5164d099da1e6ebafcf3287d
Tim O'Dell <siginthehead@gmail.com>
2022-10-21 16:16:47 +0100

    Updates to allow additional palettes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialThemeableModule } from 'material-themeable';
+import { MaterialThemeableModule } from 'dist/material-themeable';
 import { DemoSectionComponent } from './components/demo-section/demo-section.component';
 import { InfoComponent } from './components/info/info.component';
 import { WarningComponent } from './components/warning/warning.component';
 
 @NgModule({
-  declarations: [WarningComponent, InfoComponent, DemoSectionComponent],
+  declarations: [
+    WarningComponent,
+    InfoComponent,
+    DemoSectionComponent
+  ],
   exports: [
     MatToolbarModule,
     MaterialThemeableModule,
